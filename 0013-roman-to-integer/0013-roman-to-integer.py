8875fb647abfa2c389f3bb49bcbd77a3b6385012
Thimmareddy G <thimmareddygt7@gmail.com>
2026-08-23 21:15:08 +0530

    Time: 8 ms (65.31%), Space: 12.3 MB (92.35%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,5 +1,9 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         number=0
         for i in range(len(s)-1):
